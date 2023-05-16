b7062b74e0b2ed93553d7935cb3fbe4dc45d97bf
Tim Nguyen <tim.nguyen@mailplus.com.au>
2023-05-17 09:24:35 +1000

    Add NetSuite supports

---
+++ SuiteScripts/mp_cl_service_change_tn_v2_vue.js
@@ -0,0 +1,29 @@
+/**
+ * @author Tim Nguyen
+ * @description Service Change Page v2 with Vue 2. The only purpose of this script is to expose NetSuite modules to the Vue app.
+ * @NApiVersion 2.1
+ * @NScriptType ClientScript
+ * @created 16/05/2023
+ */
+
+let modules = {};
+
+const moduleNames = ['error', 'runtime', 'search', 'record', 'url', 'format', 'email', 'currentRecord'];
+
+// eslint-disable-next-line no-undef
+define(moduleNames.map(item => 'N/' + item), (...args) => {
+        for (let [index, moduleName] of moduleNames.entries())
+            modules[moduleName] = args[index];
+
+        function pageInit() {
+            console.log('Client script init.');
+        }
+
+        return { pageInit };
+    }
+);
+
+// eslint-disable-next-line no-unused-vars
+function _getClientModules() {
+    return modules;
+}
